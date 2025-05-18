d4b93c09b581e0216b8c824586365d8b763d363f
Souvik <souvik.de@sato-global.com>
2025-05-18 14:37:55 +0900

    add tabbar and icons for the tabbar

--- expense-tracker-app/app/(tabs)/index.tsx
+++ expense-tracker-app/app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import { colors } from '@/constants/theme';
 import { auth } from '@/config/firebase';
 import { signOut } from 'firebase/auth';
 import { useAuth } from '@/contexts/authContext';
+import ScreenWrapper from '@/components/ScreenWrapper';
 
 function Home() {
   const { user } = useAuth();
@@ -16,14 +17,14 @@ function Home() {
   };
 
   return (
-    <View>
-      <Text>Home</Text>
+    <ScreenWrapper>
+      <Typo>Home</Typo>
       <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
       </Button>
-    </View>
-  )
-}
+    </ScreenWrapper>
+  );
+};
 
 export default Home
 const styles = StyleSheet.create({});
